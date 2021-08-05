ed1b7148474eb312031e6b090060b7a005f675c7
sahithi28pothuri <74135534+sahithi28pothuri@users.noreply.github.com>
2021-08-05 10:13:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+
+function preload()
+{
+  
+}
+
+function setup() {
+  createCanvas(500,700);
+
+  frameRate(80);
+
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
+}
+
+function draw() 
+{
+  
+}
+
+
